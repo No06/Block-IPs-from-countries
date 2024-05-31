b02c01cd29fc5663a3de085845190ee0a416dd2e
Noo6 <2746419228@qq.com>
2024-05-31 15:40:09 +0800

    更新

--- block-ips.sh
+++ block-ips.sh
@@ -6,6 +6,9 @@
 Green="\033[32m"
 Font="\033[0m"
 
+countries=("cn")
+iplist=()
+
 #root权限
 root_need(){
     if [[ $EUID -ne 0 ]]; then
@@ -14,12 +17,31 @@ root_need(){
     fi
 }
 
-#封禁ip
-block_ipset(){
-check_ipset
+#清除iptables input规则
+clear_input_rule(){
+iptables -F INPUT
+echo -e "${Green}已清除所有INPUT规则！${Font}"
+}
+
+#清除ipset规则
+clear_ipset_rules() {
+ipset -F
+echo -e "${Green}已清除所有IPSET规则！${Font}"
+}
+
+#添加默认规则
+add_default_rules() {
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -j ACCEPT -p icmp
+iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -P INPUT DROP
+echo -e "${Green}已设置INPUT规则默认DENY${Font}"
+}
+
+#添加国家ip规则
+accept_ipset(){
 #添加ipset规则
-echo -e "${Green}请输入需要封禁的国家代码，如cn(中国)，注意字母为小写！${Font}"
-read -p "请输入国家代码:" GEOIP
+GEOIP=$1
 echo -e "${Green}正在下载IPs data...${Font}"
 wget -P /tmp http://www.ipdeny.com/ipblocks/data/countries/$GEOIP.zone 2> /dev/null
 #检查下载是否成功
@@ -34,33 +56,18 @@ wget -P /tmp http://www.ipdeny.com/ipblocks/data/countries/$GEOIP.zone 2> /dev/n
 ipset -N $GEOIP hash:net
 for i in $(cat /tmp/$GEOIP.zone ); do ipset -A $GEOIP $i; done
 rm -f /tmp/$GEOIP.zone
-echo -e "${Green}规则添加成功，即将开始封禁ip！${Font}"
+echo -e "${Green}规则添加成功，即将开始添加ip！${Font}"
 #开始封禁
-iptables -I INPUT -p tcp -m set --match-set "$GEOIP" src -j DROP
-iptables -I INPUT -p udp -m set --match-set "$GEOIP" src -j DROP
-echo -e "${Green}所指定国家($GEOIP)的ip封禁成功！${Font}"
+iptables -I INPUT -p tcp -m set --match-set "$GEOIP" src -j ACCEPT
+iptables -I INPUT -p udp -m set --match-set "$GEOIP" src -j ACCEPT
+echo -e "${Green}所指定国家($GEOIP)的ip添加成功！${Font}"
 }
 
-#解封ip
-unblock_ipset(){
-echo -e "${Green}请输入需要解封的国家代码，如cn(中国)，注意字母为小写！${Font}"
-read -p "请输入国家代码:" GEOIP
-#判断是否有此国家的规则
-lookuplist=`ipset list | grep "Name:" | grep "$GEOIP"`
-    if [ -n "$lookuplist" ]; then
-        iptables -D INPUT -p tcp -m set --match-set "$GEOIP" src -j DROP
-	iptables -D INPUT -p udp -m set --match-set "$GEOIP" src -j DROP
-	ipset destroy $GEOIP
-	echo -e "${Green}所指定国家($GEOIP)的ip解封成功，并删除其对应的规则！${Font}"
-    else
-	echo -e "${Green}解封失败，请确认你所输入的国家是否在封禁列表内！${Font}"
-	exit 1
-    fi
-}
-
-#查看封禁列表
-block_list(){
-	iptables -L | grep match-set
+#添加ip段规则
+accept_iplist(){
+IP=$1
+iptables -I INPUT -s $IP -j ACCEPT
+echo -e "${Green}所指定IP($IP)添加成功！${Font}"
 }
 
 #检查系统版本
@@ -97,30 +104,27 @@ check_ipset(){
 main(){
 root_need
 check_release
+check_ipset
+
 clear
+
+clear_input_rule
+clear_ipset_rules
 echo -e "———————————————————————————————————————"
-echo -e "${Green}Linux VPS一键屏蔽指定国家所有的IP访问${Font}"
-echo -e "${Green}1、封禁ip${Font}"
-echo -e "${Green}2、解封iP${Font}"
-echo -e "${Green}3、查看封禁列表${Font}"
+echo -e "${Green}Linux VPS一键白名单指定国家所有的IP访问${Font}"
+echo -e "${Green}指定的国家或地区[${countries[@]}]${Font}"
+echo -e "${Green}指定的IP段[${iplist[@]}]${Font}"
 echo -e "———————————————————————————————————————"
-read -p "请输入数字 [1-3]:" num
-case "$num" in
-    1)
-    block_ipset
-    ;;
-    2)
-    unblock_ipset
-    ;;
-    3)
-    block_list
-    ;;
-    *)
-    clear
-    echo -e "${Green}请输入正确数字 [1-3]${Font}"
-    sleep 2s
-    main
-    ;;
-    esac
+
+for i in ${countries[@]}; do
+	accept_ipset $i
+done
+
+for i in ${iplist[@]}; do
+	accept_iplist $i
+done
+
+#设置默认规则
+add_default_rules
 }
 main
